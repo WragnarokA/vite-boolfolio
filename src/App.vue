1ba77ec93e3afd746008bbbb4007683f3f9e2f79
WragnarokA <wilmer.arango.a@gmail.com>
2023-07-18 22:09:22 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+import AppMain from './components/AppMain.vue';
+
+export default {
+  data(){
+    return {}
+  },
+  components: {
+    AppMain
+  },
+}
+
+
+</script>
+
+<template>
+   <h1>Bentornato Wilmer </h1>
+  <AppMain />
+ 
+</template>
+
+<style scoped>
+
+</style>
