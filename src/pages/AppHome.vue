960c4249b3756ac175115f9608a460434075ae5c
WragnarokA <wilmer.arango.a@gmail.com>
2023-07-20 23:20:01 +0200

    router-links in the home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -4,6 +4,11 @@ export default {
     name: "AppHome",
     data() {
         return {
+            navLinks: [
+                { name: "home", lavel: "Home" },
+                { name: "about", lavel: "Abaut us" },
+                { name: "posts", lavel: "Posts" },
+            ]
 
         }
 
@@ -16,9 +21,26 @@ export default {
 <template>
     <section>
         <h1>Home</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio distinctio rerum deleniti totam, iure sapiente
-            quo quas, voluptates modi accusantium soluta, quae dolore eius nisi. Eligendi eaque delectus unde dolor.</p>
-        <a class="btn btn-primary m-1" href="/about-us">about</a>
-        <a class="btn btn-primary m-1" href="/posts">PostList</a>
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">NavLinks</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item" v-for="i in navLinks">
+                            <router-link :to="{ name: i.name }" class="nav-link active" aria-current="page">{{ i.lavel
+                            }}</router-link>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
     </section>
 </template>
